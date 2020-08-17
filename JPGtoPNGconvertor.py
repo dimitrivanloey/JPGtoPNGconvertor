6866b1d7dfdc4613e6608fb770009263a6aef525
dimitrivanloey <dimitrivanloey@gmail.com>
2020-08-17 13:13:02 +0100

    second commit

---
+++ JPGtoPNGconvertor.py
@@ -0,0 +1,22 @@
+import sys
+import os
+from PIL import Image
+
+# Grab first and second argument
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+# Check if new/ exists, if not create it
+if not os.path.exists((output_folder)):
+    os.makedirs(output_folder)
+
+# Loop through Pokedex, convert images to png
+# Save them to the new folder
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    clean_name = os.path.splitext(filename)[0]
+    img.save(f'{output_folder}{clean_name}.png', 'png')
+    print('All done!!')
+
+
+
